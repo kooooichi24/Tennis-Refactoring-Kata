c973aa65d2b4a082574d2a02804a93450dfc6796
kooooichi24 <ex.kouichi1224@gmail.com>
2022-04-17 16:50:19 +0900

    Revert "refactor: format"
    This reverts commit 819533760b8c718475b65f1a125dab4497cae089.

--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -1,4 +1,4 @@
-import { TennisGame } from "./TennisGame";
+import { TennisGame } from './TennisGame';
 
 export class TennisGame1 implements TennisGame {
   private m_score1: number = 0;
@@ -12,53 +12,55 @@ export class TennisGame1 implements TennisGame {
   }
 
   wonPoint(playerName: string): void {
-    if (playerName === "player1") this.m_score1 += 1;
-    else this.m_score2 += 1;
+    if (playerName === 'player1')
+      this.m_score1 += 1;
+    else
+      this.m_score2 += 1;
   }
 
   getScore(): string {
-    let score: string = "";
+    let score: string = '';
     let tempScore: number = 0;
     if (this.m_score1 === this.m_score2) {
       switch (this.m_score1) {
         case 0:
-          score = "Love-All";
+          score = 'Love-All';
           break;
         case 1:
-          score = "Fifteen-All";
+          score = 'Fifteen-All';
           break;
         case 2:
-          score = "Thirty-All";
+          score = 'Thirty-All';
           break;
         default:
-          score = "Deuce";
+          score = 'Deuce';
           break;
+
       }
-    } else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
+    }
+    else if (this.m_score1 >= 4 || this.m_score2 >= 4) {
       const minusResult: number = this.m_score1 - this.m_score2;
-      if (minusResult === 1) score = "Advantage player1";
-      else if (minusResult === -1) score = "Advantage player2";
-      else if (minusResult >= 2) score = "Win for player1";
-      else score = "Win for player2";
-    } else {
+      if (minusResult === 1) score = 'Advantage player1';
+      else if (minusResult === -1) score = 'Advantage player2';
+      else if (minusResult >= 2) score = 'Win for player1';
+      else score = 'Win for player2';
+    }
+    else {
       for (let i = 1; i < 3; i++) {
         if (i === 1) tempScore = this.m_score1;
-        else {
-          score += "-";
-          tempScore = this.m_score2;
-        }
+        else { score += '-'; tempScore = this.m_score2; }
         switch (tempScore) {
           case 0:
-            score += "Love";
+            score += 'Love';
             break;
           case 1:
-            score += "Fifteen";
+            score += 'Fifteen';
             break;
           case 2:
-            score += "Thirty";
+            score += 'Thirty';
             break;
           case 3:
-            score += "Forty";
+            score += 'Forty';
             break;
         }
       }
