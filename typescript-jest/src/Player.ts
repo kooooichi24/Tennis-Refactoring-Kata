5a3d2a9cdbd34dc8ba9aaf9ee1d16ed1221defc0
kooooichi24 <ex.kouichi1224@gmail.com>
2022-04-17 16:50:18 +0900

    Revert "refactor: 単一責任の原則に則って、TennisGameとPlayerのクラスを分けた"
    This reverts commit 76094c7a62cdce4fbda4845f2c7173377daad2ea.

--- typescript-jest/src/Player.ts
+++
@@ -1,3 +0,0 @@
-export class Player {
-  constructor(private readonly name: string) {}
-}
--- typescript-jest/src/TennisGame1.ts
+++ typescript-jest/src/TennisGame1.ts
@@ -1,15 +1,14 @@
-import { Player } from "./Player";
 import { TennisGame } from "./TennisGame";
 
 export class TennisGame1 implements TennisGame {
   private m_score1: number = 0;
   private m_score2: number = 0;
-  private player1: Player;
-  private player2: Player;
+  private player1Name: string;
+  private player2Name: string;
 
   constructor(player1Name: string, player2Name: string) {
-    this.player1 = new Player(player1Name);
-    this.player2 = new Player(player2Name);
+    this.player1Name = player1Name;
+    this.player2Name = player2Name;
   }
 
   wonPoint(playerName: string): void {
